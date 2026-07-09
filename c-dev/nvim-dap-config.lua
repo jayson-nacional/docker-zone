672808a90598173742d97805151d9ca89d671855
Jayson Nacional <jayson.o.nacional@gmail.com>
2026-07-09 21:27:42 +0800

    fix debug config for c workspace

---
+++ c-dev/nvim-dap-config.lua
@@ -0,0 +1,52 @@
+local mason_dap = require("mason-nvim-dap")
+local dap = require("dap")
+local ui = require("dapui")
+local dap_virtual_text = require("nvim-dap-virtual-text")
+
+dap_virtual_text.setup();
+
+mason_dap.setup({
+	ensure_installed = { "codelldb" },
+	automatic_installation = true,
+	handlers = {
+		function(config)
+			require("mason-nvim-dap").default_setup(config)
+		end
+	}
+})
+
+dap.adapters.codelldb = {
+	type = 'executable',
+	command = 'codelldb'
+}
+
+dap.configurations.c = {
+	{
+		type = "codelldb",
+		name = "Launch file",
+		request = "launch",
+		program = function()
+			return vim.fn.input('Path to executable', vim.fn.getcwd() .. '/', 'file')
+		end,
+		cwd = '${workspaceFolder}',
+		stopOnEntry = false,
+		args = {},
+	}
+}
+
+ui.setup()
+
+vim.fn.sign_define("DapBreakpoint", { text = "B" })
+
+dap.listeners.before.attach.dapui_config = function()
+	ui.open()
+end
+dap.listeners.before.launch.dapui_config = function()
+	ui.open()
+end
+dap.listeners.before.event_terminated.dapui_config = function()
+	ui.close()
+end
+dap.listeners.before.event_exited.dapui_config = function()
+	ui.close()
+end
